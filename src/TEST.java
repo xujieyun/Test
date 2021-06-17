c2fe30cd99a4ed77025c79cb9c9251688e479965
xujieyun <203661771@qq.com>
2021-06-18 00:40:22 +0800

    加个2

--- src/TEST.java
+++ src/TEST.java
@@ -3,7 +3,7 @@ public class TEST {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
+		System.out.print("hello world2");
 	}
 
 }
